1fb5d9e57dccb1b9e6ca389ae7798b4c1a28cd3d
shogun444 <sayandiproy17@gmail.com>
2026-04-29 00:00:50 +0530

    feat: add send gift customization view

---
+++ src/app/gift-customization/page.tsx
@@ -0,0 +1,9 @@
+import GiftCustomization from "@/components/gifting/GiftCustomization";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <GiftCustomization />
+    </div>
+  );
+}
